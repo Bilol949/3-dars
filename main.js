4e52601de40bd90c7c64de7ceb52894b22208190
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2024-12-24 18:51:09 +0500

    dars

---
+++ main.js
@@ -0,0 +1,14 @@
+// let son = Math.random()*100;
+// let natija = Math.round(son);
+// console.log(natija);
+// alert(`random son ; ${natija *3}`)
+
+alert('Hozir sizga random son chiqadi 😂')
+let son =Math.random()*100;
+let natija =Math.round(son)
+alert(`Random chiqqan son  bu : ${natija}`)
+
+confirm('Biz random soni 2 ga kopaytirmoqchi va bolmoqchimiz!')
+
+alert(`Random orqali chiqqan son: ${natija} edi \n biz buni 2 ga kopaytirib ${natija *2} \n va shu sonni 2 ga bo'lib: ${natija /2} \n va shu sonni 3 ga bolgandagi qoldiq ${natija %3} sonini chiqardik `)
+
